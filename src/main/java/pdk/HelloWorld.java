685e590754d059dc81b3ae55d9fff4b04beabbe6
JiaweiMao <jiaweimao2019@outlook.com>
2025-04-01 13:26:31 +0800

    feat:add HelloWorld

---
+++ src/main/java/pdk/HelloWorld.java
@@ -0,0 +1,12 @@
+package pdk;
+
+/**
+ * @author Jiawei Mao
+ * @version 1.0.0
+ * @since 01 Apr 2025, 1:21 PM
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
